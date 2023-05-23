c06d9a0a0feba8deae51d446f29ea1ad6f4985bb
OmryTzabbar1 <126666678+OmryTzabbar1@users.noreply.github.com>
2023-05-22 21:42:45 -0400

    Add files via upload

---
+++ TwoSum.py
@@ -0,0 +1,55 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+'''
+
+# Code:
+# Solution 1:
+def twoSum(nums, target):
+    for i, n in enumerate(nums):
+        if target - n in nums and i != nums.index(target - n):
+            return i, nums.index(target - n)
+
+# Solution 2:
+def twoSum2(nums, target):
+    prevMap = {}  # val : index
+
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
+
+ans = 15814
+
+try1 =  2 + 4 + 8 + 16 + 32 + 64 + 128 + 256 + 512 + 1024 + 2048 + 4096 + 8192
+try2 = 1024 + 2048 + 4096 + 8192
+try3 = 64 + 128 + 1024 + 2048 + 4096 + 8192 + 256 + 2 + 4
+print(try3)
